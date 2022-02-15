445adeda3f158ec1c668e8f963c005f4d3ec53d3
ertungabezirgan <ertungabezirgan@gmail.com>
2022-02-15 23:32:28 +0300

    Commit message

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: [
-    "./projects/ng-snippets-com/**/*.{html,ts}",
+    "./projects/ng-snippets-com/**/*.{html,ts,scss}",
     "./projects/ng-snippets-com/src/index.html",
   ],
   theme: {
