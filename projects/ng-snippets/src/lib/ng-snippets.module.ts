9d5e17a905ea0b94da6540a7e524d491e3bcde85
ertungabezirgan <ertungabezirgan@gmail.com>
2022-02-23 12:23:52 +0300

    Commit message

--- projects/ng-snippets/src/lib/ng-snippets.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { NgSnippetsComponent } from './ng-snippets.component';
-
-@NgModule({
-	declarations: [NgSnippetsComponent],
-	imports: [],
-	exports: [NgSnippetsComponent]
-})
-export class NgSnippetsModule {}
--- projects/ng-snippets/src/public-api.ts
+++ projects/ng-snippets/src/public-api.ts
@@ -1,7 +1,1 @@
-/*
- * Public API Surface of ng-snippets
- */
-
-export * from './lib/ng-snippets.service';
-export * from './lib/ng-snippets.component';
-export * from './lib/ng-snippets.module';
+export default {};
