03469b92b3b4b99c69065862d8cc1bc80fe3013b
ertungabezirgan <ertungabezirgan@gmail.com>
2022-02-23 13:58:42 +0300

    Commit message

--- projects/ng-snippets/src/lib/truncate/truncate.pipe.ts
+++ projects/ng-snippets/src/lib/truncate/truncate.pipe.ts
@@ -5,15 +5,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TruncatePipe implements PipeTransform {
 
-  transform(value: string, length: number, suffix: string = '...'): string {
-    if (length <= 0) {
+  transform(value: string, truncationLength: number, suffix: string = '...'): string {
+    if (truncationLength <= 0) {
       throw new Error('Truncation length must be larger than 0');
     }
 
-    if (value.length > length) {
-      return value.substring(0, length) + suffix;
-    }
-
-    return value;
+    return value.length > truncationLength ? value.substring(0, truncationLength) + suffix : value;
   }
 }
