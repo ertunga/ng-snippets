2a5e6723d2e5aecda47e9d680bd94b982be68e54
ertungabezirgan <ertungabezirgan@gmail.com>
2022-02-26 20:09:49 +0300

    Commit message

--- projects/ng-snippets/src/lib/join/join.module.ts
+++ projects/ng-snippets/src/lib/join/join.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { JoinPipe } from './join.pipe';
 
 @NgModule({
 	declarations: [JoinPipe],
-	imports: [CommonModule],
 	exports: [JoinPipe]
 })
 export class JoinModule {}
--- projects/ng-snippets/src/lib/truncate/truncate.module.ts
+++ projects/ng-snippets/src/lib/truncate/truncate.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { TruncatePipe } from './truncate.pipe';
 
 @NgModule({
 	declarations: [TruncatePipe],
-	imports: [CommonModule],
 	exports: [TruncatePipe]
 })
 export class TruncateModule {}
