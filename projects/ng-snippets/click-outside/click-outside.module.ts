286f96d75caf9b5ea00b4e8c33f6fdaf17ce8ab7
ertungabezirgan <ertungabezirgan@gmail.com>
2022-04-07 23:54:12 +0300

    feat: add click outside directive

---
+++ projects/ng-snippets/click-outside/click-outside.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClickOutsideDirective } from './click-outside.directive';
+
+@NgModule({
+	declarations: [ClickOutsideDirective],
+	imports: [CommonModule]
+})
+export class ClickOutsideModule {}
