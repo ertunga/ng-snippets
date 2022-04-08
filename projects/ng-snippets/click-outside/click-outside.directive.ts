c13f2e7e888bb96502c7fabc16bf854ba6262c2c
ertungabezirgan <ertungabezirgan@gmail.com>
2022-04-09 00:30:36 +0300

    feat: add click outside directive to library and demo app

--- projects/ng-snippets/click-outside/click-outside.directive.ts
+++ projects/ng-snippets/click-outside/click-outside.directive.ts
@@ -1,8 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, HostListener, Output } from '@angular/core';
 
 @Directive({
-	selector: '[ngsClickOutside]'
+	selector: '[clickOutside]'
 })
 export class ClickOutsideDirective {
-	constructor() {}
+	@Output() clickOutside = new EventEmitter<Event>();
+
+	constructor(private elementRef: ElementRef) {}
+
+	@HostListener('document:click', ['$event'])
+	handleClick(event: Event): void {
+		if (!this.elementRef.nativeElement.contains(event.target)) {
+			this.clickOutside.emit(event);
+		}
+	}
 }
--- projects/ng-snippets/click-outside/click-outside.module.ts
+++ projects/ng-snippets/click-outside/click-outside.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ClickOutsideDirective } from './click-outside.directive';
 
 @NgModule({
 	declarations: [ClickOutsideDirective],
-	imports: [CommonModule]
+	exports: [ClickOutsideDirective]
 })
 export class ClickOutsideModule {}
