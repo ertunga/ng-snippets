d39cadd27779fdf644e8fb0e2da23c3bc66930b3
ertungabezirgan <ertungabezirgan@gmail.com>
2022-04-27 21:52:51 +0300

    feat: add truncate words pipe

--- projects/ng-snippets-demo/src/app/app.module.ts
+++ projects/ng-snippets-demo/src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { JoinModule } from '../../../ng-snippets/join';
 import { TruncateModule } from '../../../ng-snippets/truncate';
+import { TruncateWordsModule } from '../../../ng-snippets/truncate-words';
 import { ClickOutsideModule } from '../../../ng-snippets/click-outside';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, JoinModule, TruncateModule, ClickOutsideModule],
+    imports: [BrowserModule, AppRoutingModule, JoinModule, TruncateModule, ClickOutsideModule, TruncateWordsModule],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
