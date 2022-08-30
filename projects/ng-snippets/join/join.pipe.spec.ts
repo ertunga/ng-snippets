71240a3be34c396dcfde5ec6a8d685f25a266067
ertungabezirgan <ertungabezirgan@gmail.com>
2022-08-30 15:57:05 +0300

    test: join pipe tests

--- projects/ng-snippets/join/join.pipe.spec.ts
+++ projects/ng-snippets/join/join.pipe.spec.ts
@@ -1,8 +1,28 @@
 import { JoinPipe } from './join.pipe';
 
 describe('JoinPipe', () => {
-    it('create an instance', () => {
-        const pipe = new JoinPipe();
-        expect(pipe).toBeTruthy();
+    let pipe: JoinPipe;
+    const testCases = [
+        { expectation: 'join string array', array: ['test1', 'test2', 'test3'], expected: 'test1, test2, test3' },
+        {
+            expectation: 'join string array with - separator',
+            array: ['test1', 'test2', 'test3'],
+            separator: '-',
+            expected: 'test1-test2-test3'
+        },
+        { expectation: 'join number array', array: [1, -4, 100, 0], expected: '1, -4, 100, 0' },
+        { expectation: 'join number array with - separator', array: [1, -4, 100, 0], separator: '-', expected: '1--4-100-0' },
+        { expectation: 'join empty array', array: [], expected: '' },
+        { expectation: 'join empty array with - separator', array: [], separator: '-', expected: '' }
+    ];
+
+    beforeEach(() => {
+        pipe = new JoinPipe();
+    });
+
+    testCases.forEach((testCase) => {
+        it(testCase.expectation, () => {
+            expect(pipe.transform(testCase.array, testCase?.separator)).toBe(testCase.expected);
+        });
     });
 });
