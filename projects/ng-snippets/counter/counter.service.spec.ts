9baf0b9fd70b0f5f145af7e8b1520d5b54429124
ertungabezirgan <ertungabezirgan@gmail.com>
2022-08-30 14:46:43 +0300

    test: counter service tests

--- projects/ng-snippets/counter/counter.service.spec.ts
+++ projects/ng-snippets/counter/counter.service.spec.ts
@@ -10,7 +10,38 @@ describe('CounterService', () => {
         service = TestBed.inject(CounterService);
     });
 
-    it('should be created', () => {
-        expect(service).toBeTruthy();
+    it('increase', () => {
+        service.increase();
+        expect(service.count).toBe(1);
+    });
+
+    it('increase by given number', () => {
+        service.increase(5);
+        expect(service.count).toBe(5);
+    });
+
+    it('decrease', () => {
+        service.decrease();
+        expect(service.count).toBe(-1);
+    });
+
+    it('decrease by given number', () => {
+        service.decrease(6);
+        expect(service.count).toBe(-6);
+    });
+
+    it('reset counter', () => {
+        service.count = 30;
+        service.reset();
+        expect(service.count).toBe(0);
+    });
+
+    it('returns count', () => {
+        expect(service.count).toBe(0);
+    });
+
+    it('sets count', () => {
+        service.count = 100;
+        expect(service.count).toBe(100);
     });
 });
