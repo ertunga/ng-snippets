e38c272ecd5da4bc0640201aed3eec7435d9beea
ertungabezirgan <ertungabezirgan@gmail.com>
2022-10-06 20:44:30 +0300

    test: add test for click outside directive

--- projects/ng-snippets/click-outside/click-outside.directive.spec.ts
+++ projects/ng-snippets/click-outside/click-outside.directive.spec.ts
@@ -1,10 +1,14 @@
 import { ClickOutsideDirective } from './click-outside.directive';
 import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 describe('ClickOutsideDirective', () => {
     @Component({
-        template: '<div (clickOutside)="test()"></div>'
+        template: `
+            <div (clickOutside)="test()" class="target"></div>
+            <div class="outside"></div>
+        `
     })
     class TestComponent {
         test(): void {
@@ -25,7 +29,23 @@ describe('ClickOutsideDirective', () => {
         component = fixture.componentInstance;
     });
 
-    it('should create directive', () => {
-        expect(component).toBeTruthy();
+    it('should handle click on target correctly', () => {
+        spyOn(component, 'test');
+        fixture.detectChanges();
+
+        const target = fixture.debugElement.query(By.css('.target')).nativeElement;
+        target.click();
+
+        expect(component.test).not.toHaveBeenCalled();
+    });
+
+    it('should handle click outside correctly', () => {
+        spyOn(component, 'test');
+        fixture.detectChanges();
+
+        const target = fixture.debugElement.query(By.css('.outside')).nativeElement;
+        target.click();
+
+        expect(component.test).toHaveBeenCalled();
     });
 });
