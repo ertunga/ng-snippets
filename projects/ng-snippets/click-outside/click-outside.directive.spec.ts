09ade72af62a1a6bab20c74085589b9923190c78
ertungabezirgan <ertungabezirgan@gmail.com>
2022-10-06 20:20:53 +0300

    test: add test for click outside directive

--- projects/ng-snippets/click-outside/click-outside.directive.spec.ts
+++ projects/ng-snippets/click-outside/click-outside.directive.spec.ts
@@ -1,8 +1,31 @@
 import { ClickOutsideDirective } from './click-outside.directive';
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-/*describe('ClickOutsideDirective', () => {
-    it('should create an instance', () => {
-        const directive = new ClickOutsideDirective();
-        expect(directive).toBeTruthy();
+describe('ClickOutsideDirective', () => {
+    @Component({
+        template: '<div (clickOutside)="test()"></div>'
+    })
+    class TestComponent {
+        test(): void {
+            console.log('test');
+        }
+    }
+
+    let fixture: ComponentFixture<TestComponent>;
+    let component: TestComponent;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ClickOutsideDirective, TestComponent]
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(TestComponent);
+        fixture.detectChanges();
+        component = fixture.componentInstance;
     });
-});*/
+
+    it('should create directive', () => {
+        expect(component).toBeTruthy();
+    });
+});
